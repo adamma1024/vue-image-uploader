9b5a5eed7d3bd803e8e0265faa470bd45d17da0e
qq240814476 <240814476@qq.com>
2019-06-15 23:30:29 +0800

    index

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
     <!--dom结构部分-->
     <div id="uploader-demo">
         <!--用来存放item-->
-        <div id="fileList" class="uploader-list"></div>
+        <ul id="fileList" class="uploader-list"></ul>
         <div id="filePicker">选择图片</div>
     </div>
 </template>
@@ -45,7 +45,7 @@ export default {
 
 
         // $list为容器jQuery实例
-        $list.append( $li );
+        $('#fileList').append( $li );
 
         // 创建缩略图
         // 如果为非图片文件，可以不用调用此方法。
@@ -57,9 +57,83 @@ export default {
             }
 
             $img.attr( 'src', src );
-        }, thumbnailWidth, thumbnailHeight );
+        }, 100, 100 );
+    });
+    // 文件上传过程中创建进度条实时显示。
+    uploader.on( 'uploadProgress', function( file, percentage ) {
+        var $li = $( '#'+file.id ),
+            $percent = $li.find('.progress span');
+
+        // 避免重复创建
+        if ( !$percent.length ) {
+            $percent = $('<p class="progress"><span></span></p>')
+                    .appendTo( $li )
+                    .find('span');
+        }
+
+        $percent.css( 'width', percentage * 100 + '%' );
+    });
+
+    // 文件上传成功，给item添加成功class, 用样式标记上传成功。
+    uploader.on( 'uploadSuccess', function( file ) {
+        $( '#'+file.id ).addClass('upload-state-done');
+    });
+
+    // 文件上传失败，显示上传出错。
+    uploader.on( 'uploadError', function( file ) {
+        var $li = $( '#'+file.id ),
+            $error = $li.find('div.error');
+
+        // 避免重复创建
+        if ( !$error.length ) {
+            $error = $('<div class="error"></div>').appendTo( $li );
+        }
+
+        $error.text('上传失败');
+    });
+
+    // 完成上传完了，成功或者失败，先删除进度条。
+    uploader.on( 'uploadComplete', function( file ) {
+        $( '#'+file.id ).find('.progress').remove();
     });
   }
 }
 </script>
+
+<style scoped>
+.file-item {
+    float: left;
+    position: relative;
+    margin: 0 20px 20px 0;
+    padding: 4px;
+}
+.file-item .error {
+    position: absolute;
+    top: 4px;
+    left: 4px;
+    right: 4px;
+    background: red;
+    color: white;
+    text-align: center;
+    height: 20px;
+    font-size: 14px;
+    line-height: 23px;
+}
+.file-item .info {
+    position: absolute;
+    left: 4px;
+    bottom: 4px;
+    right: 4px;
+    height: 20px;
+    line-height: 20px;
+    text-indent: 5px;
+    background: rgba(0, 0, 0, 0.6);
+    color: white;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow : ellipsis;
+    font-size: 12px;
+    z-index: 10;
+}
+</style>
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import '../static/Uploader.swf'
 
 const a = new Vue({
   template: '<App></App>',
