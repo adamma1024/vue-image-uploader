6607bc159d79a379c522be541fe8fc6c67297272
qq240814476 <240814476@qq.com>
2019-06-15 19:06:53 +0800

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
@@ -63,19 +64,27 @@ module.exports = {
   plugins: [
     new VueLoaderPlugin(),
     new CleanWebpackPlugin(),
+    new CopyWebpackPlugin([{
+      from: 'static',
+      to: 'assets',
+      toType: 'dir',
+      ignore: ['**.md', '**.gif']
+    }]),
     new HtmlWebpackPlugin({
       template: 'index.html'
     })
   ],
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
   },
   devServer: {
     contentBase: [path.join(__dirname, 'dist'), path.join(__dirname, 'static')],
     compress: true,
     port: 9000,
     hot: true,
-    open: true
+    open: true,
+    publicPath: '/'
   }
 }
